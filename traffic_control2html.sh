d819a7260f75c6e9ea4165f55d9b5cb069a4d53f
Xaver pixel Zak <59127040+pixel-sys@users.noreply.github.com>
2022-02-04 01:10:01 +0100

    Add files via upload

---
+++ traffic_control2html.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#script to run in CRON to find insert stats of traffic control into index html web page 
+
+TC_value="--\>$(tc qdisc show dev ens18)"
+
+sed "s/--.*/$TC_value/" /usr/share/nginx/html/index.html > /usr/share/nginx/html/tmp
+cp /usr/share/nginx/html/tmp  /usr/share/nginx/html/index.html
+rm /usr/share/nginx/html/tmp
